6a98631704350e1b2fdc3c5183c9096b3562814a
Dasha <dasha.kotova.mail@gmail.com>
2021-12-07 20:11:04 -0500

    re-add angular basics

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Moarformz
+# Formz
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.4.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/moarformz'),
+      dir: require('path').join(__dirname, './coverage/formz'),
       subdir: '.',
       reporters: [
         { type: 'html' },
